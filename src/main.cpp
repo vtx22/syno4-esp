300eeb3ea38dcf21c0a7739ac619c887b8a23f6d
Fynn Raddatz <fynn.raddatz@gmail.com>
2022-01-28 20:03:49 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+// Software for SYNO4 - ESP8266 Software
+//
+// 02/2022
+
+#include <Arduino.h>
+
+bool scanData[360]; //Stores a full LIDAR scan, distance for every angle from 0 - 360°
+
+void setup()
+{
+   // put your setup code here, to run once:
+}
+
+void loop()
+{
+   // put your main code here, to run repeatedly:
+}
